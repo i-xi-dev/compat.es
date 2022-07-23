6a5f4774fec32211cd4e9f320a2a733248b1f97f
i-xi-dev <naoyuki.isono@gmail.com>
2022-07-23 11:50:00 +0900

    1.1.2

--- src/transform_stream.ts
+++ src/transform_stream.ts
@@ -1,3 +1,19 @@
+/**
+ * If the `globalThis` has a [`TransformStream`](https://developer.mozilla.org/en-US/docs/Web/API/TransformStream), or if the `node:stream/web` package has a `TransformStream`, then the `TransformStream` constructor.
+ * Otherwise referencing it will throw a `ReferenceError`.
+ *
+ * - Browser  
+ * References the `globalThis.TransformStream`
+ *
+ * - Deno  
+ * References the `globalThis.TransformStream`
+ *
+ * - Node.js 18.0+  
+ * References the `globalThis.TransformStream`
+ * 
+ * - Node.js 15.7+ or 14.18+  
+ * References the [`TransformStream`](https://nodejs.org/api/webstreams.html#class-transformstream) of `node:stream/web` package
+ */
 let _TransformStream: {
   prototype: TransformStream;
   new <I, O>(
