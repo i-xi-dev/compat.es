69c47162a375068b0045cdb6e9a91ddc68131a57
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-09 23:21:56 +0900

    1.2.14

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export {
   RoundingMode,
   SafeInteger,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/5.4.2/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/6.0.0/mod.ts";
